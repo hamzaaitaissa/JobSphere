af9f3c919639dec7cf27db19818e9c01da6a7bfd
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2025-03-30 00:48:58 +0000

    Dev: User Repository

---
+++ Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using JobSphere.Entities;
+
+namespace JobSphere.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByIdAsync(int id);
+        Task<User> GetByUserNameAsync(string FullName);
+        Task<IEnumerable<User>> GetAllAsync();
+        Task UpdateAsync(User user);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using JobSphere.Data;
+using JobSphere.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobSphere.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly JobSphereContext _jobSphereContext;
+
+        public UserRepository(JobSphereContext jobSphereContext)
+        {
+            _jobSphereContext = jobSphereContext;
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var user = await _jobSphereContext.Users.FindAsync(id);
+            if (user != null)
+            {
+                _jobSphereContext.Users.Remove(user);
+                await _jobSphereContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            var users = await _jobSphereContext.Users.ToListAsync();
+            return users;
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            var user = await _jobSphereContext.Users.FindAsync(id);
+            if (user != null)
+            {
+                return user;
+            }
+            throw new KeyNotFoundException($"User with ID {id} not found.");
+        }
+
+        public async Task<User> GetByUserEmailAsync(string Email)
+        {
+            var user = await _jobSphereContext.Users.FindAsync($"{Email}");
+            if (user != null)
+            {
+                return user;
+            }
+            throw new KeyNotFoundException($"User with email {Email} not found.");
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _jobSphereContext.Users.Update(user);
+            await _jobSphereContext.SaveChangesAsync();
+        }
+    }
+}
