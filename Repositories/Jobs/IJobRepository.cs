d896d0fa2aa06744e75e37f339a8f6c51ce0f04a
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2025-04-06 13:02:05 +0100

    Dev: job repo interface

---
+++ Repositories/Jobs/IJobRepository.cs
@@ -0,0 +1,13 @@
+﻿using JobSphere.Entities;
+
+namespace JobSphere.Repositories.Jobs
+{
+    public interface IJobRepository
+    {
+        Task<Job> CreateAsync(Job job);
+        Task<IEnumerable<Job>> GetAllAsync();
+        Task<Job> GetByIdAsync(int id);
+        Task UpdateAsync(int id, Job job);
+        Task DeleteAsync(int id);
+    }
+}
