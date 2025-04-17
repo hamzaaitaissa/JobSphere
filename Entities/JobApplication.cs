f9d0a417f535c7f484cd6a49fd3a094654b64300
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2025-04-17 23:48:09 +0100

    dev: added JobApplication

---
+++ Entities/JobApplication.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobSphere.Entities
+{
+    public class JobApplication
+    {
+        public int Id { get; set; }
+        public int JobId { get; set; }
+        public Job Job { get; set; }
+
+        public int ApplicantId { get; set; }
+        public User Applicant { get; set; }
+
+        public string CvPath { get; set; }  
+
+        public DateTime UploadedAt { get; set; }
+    }
+}
